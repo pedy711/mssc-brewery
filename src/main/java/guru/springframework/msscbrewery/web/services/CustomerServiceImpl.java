9dc9dcc50595b6eb65df2197acf9d79e0337ac13
middoors <pedram.khoshdani@middoors.com>
2021-08-28 13:17:36 +0200

    API Versioning example

---
+++ src/main/java/guru/springframework/msscbrewery/web/services/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package guru.springframework.msscbrewery.web.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name("Pedram Khoshdani")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo impl - would add a real imlp to update customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
+    }
+
+
+}
